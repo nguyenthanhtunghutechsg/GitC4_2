467e93f0cdc1a8c49357d3f53f77da631f78eab9
Tungnt <nguyenthanhtunghutechsg@gmail.com>
2023-12-20 12:51:43 +0700

    Create Lecture.cs in folder Models

---
+++ Demo/Models/Lecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Lecture:User
+    {
+		private int mSGV;
+
+		public int MSGV
+		{
+			get { return mSGV; }
+			set { mSGV = value; }
+		}
+
+	}
+}
